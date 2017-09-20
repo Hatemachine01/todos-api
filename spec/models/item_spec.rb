fe274e3094906e140fa401c5264a7ee73f0e54df
Julio <juliodevasquez@gmail.com>
2017-09-20 16:51:24 -0500

    changed gemfile for production

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,21 @@
+# spec/models/item_spec.rb
+require 'rails_helper'
+
+# Test suite for the Item model
+RSpec.describe Item, type: :model do
+  # Association test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:todo) }
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
+end
+
+# app/models/item.rb
+class Item < ApplicationRecord
+  # model association
+  belongs_to :todo
+
+  # validation
+  validates_presence_of :name
+end
